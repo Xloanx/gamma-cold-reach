b513d67944961d9bf1e9fbb612c41cb0f4c97d6f
Abiodun Muh'd-Ahmad Odukaye <abiodun.odukaye@mykinetic.app>
2025-03-13 19:20:27 +0100

    implemented routes for call

---
+++ app/routes/call.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, HTTPException, Query
+from pydantic import BaseModel, Field, EmailStr
+from app.services.call_services import make_phone_call
+from app.models.model import CallRequest
+
+class CallRequest(BaseModel):
+    recipient_phone: str
+    call_script: str
+
+router = APIRouter()
+
+
+@router.post("/call")
+def send_cold_call(request: CallRequest):
+    try:
+        make_phone_call(
+            request.recipient_phone,
+            request.call_script
+        )
+        return "Success"
+    except Exception as e:
+        return "Failure"
+    
